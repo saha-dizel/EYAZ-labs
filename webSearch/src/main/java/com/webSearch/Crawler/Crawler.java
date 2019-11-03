5f425038d95a851ec39a29f41897ae71e2761d3b
Aliaksandr Kharavets <saha.dizel@gmail.com>
2019-11-03 20:50:50 +0300

    Added dependency for elasticsearch, also some prep for working with elastic

--- webSearch/src/main/java/com/webSearch/Crawler/Crawler.java
+++ webSearch/src/main/java/com/webSearch/Crawler/Crawler.java
@@ -4,12 +4,16 @@ import edu.uci.ics.crawler4j.crawler.*;
 import edu.uci.ics.crawler4j.parser.*;
 import edu.uci.ics.crawler4j.url.*;
 
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 import java.util.Set;
 import java.util.regex.Pattern;
 
 public class Crawler extends WebCrawler {
     private final static Pattern FILTERS = Pattern.compile(".*(\\.(css|js|gif|jpg|png|mp3|mp4|zip|gz))$");
-    private final static Pattern TUTMATCHER = Pattern.compile("^https?:\\/{2}.*\\.?tut\\.by.*$");
+    private final static Pattern TUTMATCHER = Pattern.compile("^https?:/{2}.*\\.?tut\\.by.*$");
 
     @Override
     public boolean shouldVisit(Page referringPage, WebURL url) {
@@ -24,19 +28,34 @@ public class Crawler extends WebCrawler {
 
     @Override
     public void visit(Page page) {
-        String url = page.getWebURL().getURL();
-        System.out.println("URL: " + url);
+        try {
+            URL url = new URL(page.getWebURL().getURL());
+            System.out.println("URL: " + page.getWebURL().getURL());
 
-        if (page.getParseData() instanceof HtmlParseData) {
-            HtmlParseData htmlParseData = (HtmlParseData) page.getParseData();
-            String text = htmlParseData.getText();
-            String html = htmlParseData.getHtml();
-            Set<WebURL> links = htmlParseData.getOutgoingUrls();
+            if (page.getParseData() instanceof HtmlParseData) {
+                HtmlParseData htmlParseData = (HtmlParseData) page.getParseData();
+                String text = htmlParseData.getText();
+                String html = htmlParseData.getHtml();
+                Set<WebURL> links = htmlParseData.getOutgoingUrls();
 
-            //TODO: add work with page here (analysis of text)
-            System.out.println("Text length: " + text.length());
-            System.out.println("Html length: " + html.length());
-            System.out.println("Number of outgoing links: " + links.size());
+                System.out.println("Text length: " + text.length());
+                System.out.println("Html length: " + html.length());
+                System.out.println("Number of outgoing links: " + links.size());
+
+                //TODO: rework this into elsaticsearch api (maybe delete HttpURLConnection after this)
+                HttpURLConnection con = (HttpURLConnection)url.openConnection();
+                con.setRequestMethod("PUT");
+
+                con.setDoOutput(true);
+
+                OutputStreamWriter out = new OutputStreamWriter(
+                        con.getOutputStream()
+                );
+
+            }
+        }
+        catch (Exception e) {
+            e.printStackTrace();
         }
     }
 }
